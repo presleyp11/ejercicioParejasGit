53c66078e88a864d721b5bdb9398f85bc0e48cef
Sergio <sergio.seblac@gmail.com>
2021-11-10 10:51:34 +0100

    Creo la parte de HTML

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-    echo "<h1>Ejercicio Git en parejas: Sergio y Presley</h1>";
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio Parejas</title>
+</head>
+<body>
+    <h1>Ejercicio Git en parejas: Sergio y Presley</h1>
+    <form method="post" action="index.php"> 
+        <input type="text" name="nombre" placeholder="Nombre" required="required" /> 
+        <input type="password" name="contrasena" placeholder="Contraseña" required="required" /> 
+        <button type="submit">Pulsame</button> 
+    </form>
+    <?php
+        echo "";
+    ?>
+</body>
+</html>
