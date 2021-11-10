858569b1d88aeb39cb5b49ba7e7559ef98051119
presley <presley.palcru@educa.jcyl.es>
2021-11-10 11:29:43 +0100

    parte de php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
     echo "<h1>Ejercicio Git en parejas: Sergio y Presley</h1>";
+    if(isset($_POST['contrasena'])){
+        if($_POST['contrasena']=="daw2"){
+            echo "Bienvenido $_POST['nombre']";
+        }
+    }
 ?>
